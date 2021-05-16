03490b61697683e16e1a5a65119443ebde3b9333
Reshma <reshma123rhyme@gmail.com>
2021-05-15 23:41:26 -0400

    more

---
+++ read.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Author: Reshma Shriyan
+# Date created: 05/15/2021
+
+# Description: Read from user
+
+read -p "Input your first name: "  name
+read -p "Input your age: " age
+read -p "Input your town: "  town
+echo "Hello $name. Your age is $age. You are from $town"
+
